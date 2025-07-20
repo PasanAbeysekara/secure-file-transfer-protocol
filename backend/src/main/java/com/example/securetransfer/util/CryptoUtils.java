2783c3ef8723f7464bda691a47d4d5584721d922
Pasan Kavinda Abeysekara <69195287+PasanAbeysekara@users.noreply.github.com>
2025-07-20 06:19:33 +0000

    Add CryptoUtils class for cryptographic operations including hashing, encryption, and signing

---
+++ backend/src/main/java/com/example/securetransfer/util/CryptoUtils.java
@@ -0,0 +1,60 @@
+package com.example.securetransfer.util;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
+
+public class CryptoUtils {
+
+    public static byte[] calculateSHA256Hash(byte[] data) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        return digest.digest(data);
+    }
+
+    public static byte[] aesEncrypt(byte[] data, SecretKey key, IvParameterSpec iv) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        return cipher.doFinal(data);
+    }
+
+    public static byte[] aesDecrypt(byte[] encryptedData, SecretKey key, IvParameterSpec iv) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        return cipher.doFinal(encryptedData);
+    }
+
+    public static byte[] signData(byte[] data, PrivateKey privateKey) throws Exception {
+        Signature signer = Signature.getInstance("SHA256withRSA");
+        signer.initSign(privateKey);
+        signer.update(data);
+        return signer.sign();
+    }
+
+    public static boolean verifySignature(byte[] data, byte[] signature, PublicKey publicKey) throws Exception {
+        Signature verifier = Signature.getInstance("SHA256withRSA");
+        verifier.initVerify(publicKey);
+        verifier.update(data);
+        return verifier.verify(signature);
+    }
+
+    public static byte[] rsaEncrypt(byte[] data, Key key) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(data);
+    }
+
+    public static byte[] rsaDecrypt(byte[] encrypted, Key key) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(encrypted);
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte b : bytes) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
+}
